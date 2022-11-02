72f5e68621ae7d6db208d248b80b13c8280ac50e
Paulo Roberto <paulo.trabalho10@gmail.com>
2022-11-01 21:35:11 -0300

    Create Aula007.py
    crie uma função com 2 parâmetros valor do imposto e o valor do produto de a soma.

---
+++ Aula007.py
@@ -0,0 +1,8 @@
+# crie uma função com 2 parâmetros valor do imposto e o valor do produto de a soma.
+
+def alterarValor(porc , valor):
+    return ((porc / 100) * valor) + valor
+
+
+
+print(alterarValor(10,100))
